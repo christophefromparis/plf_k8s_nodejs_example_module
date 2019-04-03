d950c5cceababc2543f57e902221783f720e4f09
Christophe COSNEFROY <christophe.cosnefroy.ext@veolia.com>
2019-04-03 07:07:30 +0000

    Add log

--- app.js
+++ app.js
@@ -11,4 +11,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`plf_k8s_nodejs_example`);
 });
