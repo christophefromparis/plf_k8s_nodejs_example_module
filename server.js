bf64d68aefb6b5fa1d30e887d8bd09ddcccf525b
christophefromparis <christophe.cosnefroy@gmail.com>
2019-04-03 11:05:08 +0200

    Typo

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log('Server running at http://${hostname}:${port}/');
   console.log('plf_k8s_nodejs_example'');
-  console.log('I am here');
+  console.log('I am here...');
 });
