f671db23e9b0db1b5d5c6491992dd25c248f8d9c
christophefromparis <christophe.cosnefroy@gmail.com>
2019-04-11 17:48:24 +0200

    Not yet

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const port = 9102;
 
 collectDefaultMetrics({ timeout: 5000 });
 
-var express = require('express')
+var express = require('express');
 var session = require('express-session');
 var Keycloak = require('keycloak-connect');
 
-var app = express()
+var app = express();
 
 var memoryStore = new session.MemoryStore();
 var keycloak = new Keycloak({ store: memoryStore });
@@ -25,21 +25,19 @@ app.use(session({
   store: memoryStore
 }));
 
-app.use( keycloak.middleware() );
+app.use( keycloak.middleware( { logout: '/'} ));
 
 app.get('/', function (req, res) {
   counter.inc();
   res.send('Hello the world')
-})
+});
 
 app.get('/metrics', function (req, res) {
   res.send(prometheus.register.metrics())
-})
+});
 
 app.get( '/protected', keycloak.protect(), (req, res) => {
   res.send('Welcome to the protected content!')
-})
+});
 
-app.use( keycloak.middleware( { logout: '/'} ))
-
-app.listen(port)
+app.listen(port);
